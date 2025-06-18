960f54511102ea928403e8c35ab514634e5b948d
Mauzy0x <mauzy.mail@proton.me>
2025-06-18 17:36:15 -0400

    init

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+/*
+*   Purpose: A learning project to write my own directory enumeration tool.
+*               Pass a hash value and a word list to crack hashed passwords!
+*               Can also be used to quickly generate hashes of a wordlist (not implemented yet)
+*
+*   Author: Mauzy0x00
+*   Date:   6.18.2025
+*
+*/
+
+// IO
+use std::fs::File;
+use std::path::PathBuf;
+
+use clap::Parser;
+
+// use std::time::Duration;
+
+// CLI
+use anyhow::Result;
+use log::{info, warn};
+
+fn main() -> Result<()> {
+
+    Ok(())
+}
