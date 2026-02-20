5fc41735ca69c8324947e87ef5ff9fdd05373b73
Mauzy0x00 <mauzy.mail@proton.me>
2026-02-19 23:18:04 -0500

    remove unused function

--- src/main.rs
+++ src/main.rs
@@ -137,7 +137,7 @@ fn enumerate(
         println!("[+] Building threads...");
     }
 
-    // Creeate a struct of the progress bar(s) to be shared across threads and add the progress bar to it
+    // Create a struct of the progress bar(s) to be shared across threads and add the progress bar to it
     let multi_progress = MultiProgress::new();
 
     let style = ProgressStyle::with_template(
@@ -375,31 +375,6 @@ fn initialize() {
 
     println!("{banner}");
 }
-
-fn process_wordlist(wordlist_path: PathBuf, thread_count: usize) -> Result<u64> {
-        println!("[+] Processing the wordlist");
-    // Open the path in read-only mode, returns `io::Result<File>`
-    let wordlist_file = match File::open(&wordlist_path) {
-        Err(why) => panic!("couldn't open {}: {}", wordlist_path.display(), why),
-        Ok(file) => file,
-    };
-    // Get the size of the input wordlist
-    let file_size = wordlist_path.metadata().unwrap().len();
-    println!("File size: {file_size}");
-
-    // Probably a better way to do this... proof of concept atm
-    let file = File::open("combined_words.txt")?;
-    let reader = BufReader::new(file);
-
-    let mut wordlist_line_count = 0;
-    for _ in reader.lines() {
-        wordlist_line_count += 1;
-    }
-
-    let partition_size = file_size / thread_count as u64; // Get the  size of each thread partition
-
-    Ok(partition_size)
-} 
 
 use std::io::{self, BufRead, BufReader, Seek, SeekFrom};
 /// Count how many lines are in the portion of the file that was partitioned to each thread
